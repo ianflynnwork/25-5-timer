7d770aaca3381518c02e693b487e83f68809dc46
ianflynnwork <ianflynnwork@gmail.com>
2022-07-15 12:29:02 -0400

    setting up handleReset

--- src/Timer.js
+++ src/Timer.js
@@ -6,9 +6,12 @@ class Timer extends React.Component{
         super();
         this.state={
             breakLength: 5,
-            sessionLength: 25
-        }
+            sessionLength: 25,
+            timeLeft: 80085
+        };
+        this.handleReset = this.handleReset.bind(this);
     }
+    handleReset 
 
     render(){
         return(
@@ -35,7 +38,12 @@ class Timer extends React.Component{
                     <button id="session-decrement">Down</button>
                     <button id="session-increment">Up</button>
                 </div>
-                
+                <div>
+                    <div id="timer-label">Session</div>
+                    <div id="time-left">{this.state.timeLeft}</div>
+                    <button id="start_stop">Start/Stop</button>
+                    <button id="reset" onClick = {this.handleReset}>Reset</button>
+                </div>
                 
             </div>
         )
