3c9a3d52cead5753e5cee1299c6aa607fb71b147
ianflynnwork <ianflynnwork@gmail.com>
2022-07-18 16:08:18 -0400

    set times for seconds and mins only

--- src/Timer.js
+++ src/Timer.js
@@ -3,9 +3,7 @@ import { getRemainingTimeUntilMsTimestamp } from "./TimerUtils";
 
 const defaultRemainingTime = {
     seconds: '00',
-    minutes: '00',
-    hours: '00',
-    days: '00'
+    minutes: '00'
 }
 
 const Timer = ({countdownTimestampMs}) => {    
@@ -23,10 +21,6 @@ const Timer = ({countdownTimestampMs}) => {
     }
     return(
         <div> 
-            <span>{remainingTime.days}</span>
-            <span>days</span>
-            <span>{remainingTime.hours}</span>
-            <span>hours</span>
             <span>{remainingTime.minutes}</span>
             <span>minutes</span>
             <span>{remainingTime.seconds}</span>
--- src/TimerUtils.js
+++ src/TimerUtils.js
@@ -7,15 +7,11 @@ export function getRemainingTimeUntilMsTimestamp(timestampMs){
         return {
             seconds: '00',
             minutes: '00',
-            hours: '00',
-            days: '00'
         }
     }
     return {
         seconds: getRemainingSeconds(nowDayjs, timestampsDayjs),
-        minutes: getRemainingMinutes(nowDayjs, timestampsDayjs),
-        hours: getRemainingHours(nowDayjs, timestampsDayjs),
-        days: getRemainingDays(nowDayjs, timestampsDayjs)
+        minutes: getRemainingMinutes(nowDayjs, timestampsDayjs)
     };
 }
 function getRemainingSeconds(nowDayjs, timestampsDayjs){
@@ -26,17 +22,8 @@ function getRemainingMinutes(nowDayjs, timestampsDayjs){
     const minutes = timestampsDayjs.diff(nowDayjs, 'minutes') % 60;
     return padWithZeros(minutes, 2);
 }
-function getRemainingHours(nowDayjs, timestampsDayjs){
-    const hours = timestampsDayjs.diff(nowDayjs, 'hours') % 24;
-    return padWithZeros(hours, 2);
-}
-function getRemainingDays(nowDayjs, timestampsDayjs){
-    const days = timestampsDayjs.diff(nowDayjs, 'days') % 60;
-    return days.toString();
-}
 function padWithZeros(number, minLength){
     const numberString = number.toString();
     if(numberString.length >= minLength) return numberString;
     return '0'.repeat(minLength - numberString.length) + numberString;
-
 }
