23864e00a99f5416ff2c51de241100258c338ecc
ianflynnwork <ianflynnwork@gmail.com>
2022-07-18 13:33:29 -0400

    starting differently

--- src/Timer.js
+++ src/Timer.js
@@ -1,58 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const defaultRemainingTime = {
-    breakLength: 5,
-    sessionLength: 25,
-    minutes: '25',
-    seconds: '00'
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days: '00'
 }
 
 const Timer = () => {    
-    const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
-
+    
         return(
             <div> 
-                <div>
-                    <div id="break-label">Break Length</div>
-                    <input 
-                        id="break-length"
-                        type="text"
-                        readOnly 
-                        value={remainingTime.breakLength}
-                    />
-                    <button id="break-decrement">Down</button>
-                    <button id="break-increment">Up</button>
-                </div>
-                <div>
-                    <div id="session-label">Session Length</div>
-                    <input 
-                        id="session-length"
-                        type="text"
-                        readOnly 
-                        value={remainingTime.sessionLength}
-                    />
-                    <button id="session-decrement">Down</button>
-                    <button id="session-increment">Up</button>
-                </div>
-                <div background-color="">
-                    <div id="timer-label">Session</div>
-                    
-                    
-                    <div id="time-left">
-                    <span>{remainingTime.minutes}</span>
-                    <span>:</span>
-                    <span>{remainingTime.seconds}</span>
-                    
-                    
-                    
-                    </div>
-                    
-                    
-                    
-                    <button id="start_stop">Start/Stop</button>
-                    <button id="reset">Reset</button>
-                </div>
-                
+                <span>30</span>
+                <span>days</span>
+                <span>12</span>
+                <span>hours</span>
+                <span>4</span>
+                <span>minutes</span>
+                <span>32</span>
+                <span>seconds</span>
+
             </div>
         )
     
