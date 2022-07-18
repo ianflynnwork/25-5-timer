bcd0a9ae3c3efd4c4ca6a7389d76df41a2dbb6b9
ianflynnwork <ianflynnwork@gmail.com>
2022-07-18 16:09:29 -0400

    delete day.js implementation

--- src/Timer.js
+++ src/Timer.js
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
-import { getRemainingTimeUntilMsTimestamp } from "./TimerUtils";
 
-const defaultRemainingTime = {
-    seconds: '00',
-    minutes: '00'
-}
-
-const Timer = ({countdownTimestampMs}) => {    
-    const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
-
-    useEffect(()=>{
-        const intervalID = setInterval(()=>{
-            updateRemainingTime(countdownTimestampMs);
-        },1000);
-        return () => clearInterval(intervalID);
-    },[countdownTimestampMs])
-
-    function updateRemainingTime(countdown){
-        setRemainingTime(getRemainingTimeUntilMsTimestamp(countdown));
-    }
+const Timer = () => {    
+    
     return(
         <div> 
-            <span>{remainingTime.minutes}</span>
-            <span>minutes</span>
-            <span>{remainingTime.seconds}</span>
-            <span>seconds</span>
-
+            hey girl
         </div>
     )
     
