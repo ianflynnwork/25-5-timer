3e21a37fef0aee966f9a2a869ebb16c7f33e2eda
ianflynnwork <ianflynnwork@gmail.com>
2022-07-20 14:30:55 -0400

    length button functionality

---
+++ src/Length.js
@@ -0,0 +1,18 @@
+
+
+const Length = ({title, changeTime, type, time, formatTime}) => {
+
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div className="times-sets">
+                <button onClick={() => changeTime(-60, type)}>Down</button>
+            </div>
+            <h3>{formatTime(time)}</h3>
+            <div className="times-sets">
+                <button>Up</button>
+            </div>
+        </div>
+    )
+}
+export default Length;
--- src/Timer.js
+++ src/Timer.js
@@ -1,14 +1,28 @@
 import { useState } from "react";
+import Length from './Length';
 
 const Timer = () => {  
     const [displayTime, setDisplayTime] = useState(25*60);
+    const [breakTime, setBreakTime] = useState(5*60);
+
     const formatTime = (time) => {
         const minutes = Math.floor(time/60);
         const seconds = time % 60;
         return (minutes < 10 ? '0' + minutes: minutes) + ':' + (seconds < 10 ? '0' + seconds: seconds)
     };
+    const changeTime = (amount, type) => {
+        if(type == 'break'){
+            setBreakTime((prev)=> prev + amount)
+        }
+    }
     return(
         <div className="timer">
+            <Length 
+                title={'break length'} 
+                changeTime={changeTime}
+                type={'break'}
+                time={breakTime}
+                formatTime={formatTime}/>
             <h1>{formatTime(displayTime)}</h1>
         </div>
     )
