4033015f118948fff488b29f2938823a404387fa
ianflynnwork <ianflynnwork@gmail.com>
2022-07-23 16:22:02 -0400

    update css to match other project

--- src/Timer.js
+++ src/Timer.js
@@ -8,17 +8,15 @@ const Timer = () => {
     const [sessionTime, setSessionTime] = useState(25*60);
     const [timerOn, setTimerOn] = useState(false);
     const [onBreak, setOnBreak] = useState(false);
-    const breakAudio = new Audio(alarmSound);
-
-//new
     const [intervalId, setIntervalId] = useState(null);
+    const breakAudio = new Audio(alarmSound);
 
     useEffect(() => {
         setDisplayTime(sessionTime);
       }, [sessionTime]);
 
     useEffect(()=>{
-        if (displayTime == 0){
+        if (displayTime === 0){
             breakAudio.currentTime = 1;
             breakAudio.play();
             setTimeout(()=>{
@@ -34,33 +32,24 @@ const Timer = () => {
             }
         }
     }, [displayTime, onBreak, breakTime, sessionTime]);
-//end new
-
-    const playBreakSound = () => {
-        breakAudio.currentTime = 1;
-        breakAudio.play();
-        setTimeout(()=>{
-            breakAudio.pause();
-            breakAudio.currentTime = 0;
-        }, 1300);
-    }
     const formatTime = (time) => {
         let minutes = Math.floor(time/60);
         let seconds = time % 60;
         return (minutes < 10 ? '0' + minutes: minutes) + ':' + (seconds < 10 ? '0' + seconds: seconds)
     };
+
     const changeTime = (amount, type) => {
         if(timerOn){
             return;
         }
-        if(type == 'break'){
+        if(type === 'break'){
             //keep timers at 1 minute or higher
             if(breakTime <= 60 && amount < 0 ){
                 return;
             }
             setBreakTime((prev)=> prev + amount)
         }
-        if(type == 'session'){
+        if(type === 'session'){
             //keep timers at 1 minute or higher
             if(sessionTime <= 60 && amount < 0){
                 return;
@@ -87,51 +76,56 @@ const Timer = () => {
             clearInterval(intervalId);
             setIntervalId(null);
         }
+        setTimerOn(!timerOn);
     };
 
     const resetTime = () => {
-        breakAudio.pause();
         clearInterval(intervalId);
         setIntervalId(null);
         setDisplayTime(25*60);
         setBreakTime(5*60);
         setSessionTime(25*60);
         setOnBreak(false);
+        setTimerOn(false);
     };
 
     return(
-        <div className="timer">
+        <div className="timer background">
             <h1>Pomodoro Clock</h1>
-            <button onClick={playBreakSound}>Play</button>
-            <div className="dual-containers">
-            <div>
-                <h3 id='break-label'>Break Length</h3>
-                <div className="times-sets">
-                    <button onClick={() => changeTime(-60, 'break')} id='break-decrement'>Down</button>
+            {/* <button onClick={playBreakSound}>Play</button> */}
+            <div className="contents">
+                <div id='break-box' className="stripe">
+                    <div className="orb left">
+                        <h3 id='break-label'>Break Length</h3>
+                        <button onClick={() => changeTime(-60, 'break')} id='break-decrement'><i className="fa-solid fa-arrow-down"></i></button>
+                        <span id='break-length'>{breakTime/60}</span>
+                        <button onClick={() => changeTime(60, 'break')} id='break-increment'><i className="fa-solid fa-arrow-up"></i></button>
+                    </div>
                 </div>
-                <h3 id='break-length'>{breakTime/60}</h3>
-                <div className="times-sets">
-                <button onClick={() => changeTime(60, 'break')} id='break-increment'>Up</button>
+                <div id='session-box' className="stripe">
+                    <div className="orb right">
+                        <h3 id='session-label'>Session Length</h3>
+                        <button onClick={() => changeTime(-60, 'session')} id='session-decrement'><i className="fa-solid fa-arrow-down"></i></button>
+                        <span id='session-length'>{sessionTime/60}</span>
+                        <button onClick={() => changeTime(60, 'session')} id='session-increment'><i className="fa-solid fa-arrow-up"></i></button>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h3 id='session-label'>Session Length</h3>
-                <div className="times-sets">
-                    <button onClick={() => changeTime(-60, 'session')} id='session-decrement'>Down</button>
+                <div id='timer-box'>
+                    <h3 id='timer-label'>{onBreak ? 'Break' : 'Session'}</h3>
+                    <h1 id='time-left'>{formatTime(displayTime)}</h1>
+                    <div id="button-box">
+                        <button id='start_stop' onClick={timerControl}>{timerOn ? <i className="fa-solid fa-pause"></i>:<i className="fa-solid fa-play"></i>}</button>
+                        <button id='reset' onClick={resetTime}><i className="fa-solid fa-arrows-rotate"></i></button>
+                    </div>
+                    <audio
+                        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+                        id="beep"
+                        ref={breakAudio}>
+                    </audio>
                 </div>
-                <h3 id='session-length'>{sessionTime/60}</h3>
-                <div className="times-sets">
-                <button onClick={() => changeTime(60, 'session')} id='session-increment'>Up</button>
-                </div>
-            </div>
-            </div>
-            <h3 id='timer-label'>{onBreak ? 'Break' : 'Session'}</h3>
-            <h1 id='time-left'>{formatTime(displayTime)}</h1>
-            <button id='start_stop' onClick={timerControl}>{timerOn ? ('Pause'):('Start')}</button>
-            <button id='reset' onClick={resetTime}>Reset</button>
+            </div> 
         </div>
-    )
-    
+    )   
 }
 
 export default Timer;
